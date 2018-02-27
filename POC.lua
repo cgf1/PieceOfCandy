5d0085f39ad2360b0ae7004a97ce5d6515764451
Chris Faylor <cgf@realedsolutions.com>
2018-02-26 23:53:54 -0500

    * POC.lua: Move string initialization here. * i18n/*: Delete (for now).

--- POC.lua
+++ POC.lua
@@ -16,8 +16,8 @@ POC_REAL_API_VERSION = 0
 POC_API_VERSION = POC_REAL_API_VERSION
 
 local MAJOR = "2"
-local MINOR = "3"
-local PATCH = "4"
+local MINOR = "4"
+local PATCH = "0"
 
 POC = {
     Name = "POC"
@@ -29,6 +29,34 @@ POC.__index = POC
 function POC:initialize()
     -- Initialize logging
     df("Piece of Candy! (v%d.%d.%d)", MAJOR, MINOR, PATCH)
+
+    local strings = {
+	POC_WARNING_LGS_ENABLE =             "Warning: Enable LibGroupSocket to send -> /lgs 1",
+	POC_OPTIONS_HEADER =                 "Options",
+	POC_OPTIONS_DRAG_LABEL =             "Drag elements",
+	POC_OPTIONS_DRAG_TOOLTIP =           "If activated, you can drag all elements.",
+	POC_OPTIONS_ONLY_AVA_LABEL =         "Show only in AvA",
+	POC_OPTIONS_ONLY_AVA_TOOLTIP =       "If activated, all elements will only be visible in Cyrodiil (AvA).",
+	POC_OPTIONS_USE_LGS_LABEL =          "Communication via LibGroupSocket",
+	POC_OPTIONS_USE_LGS_TOOLTIP =        "If activated, the addon will try to activate communication via LibGroupSocket. LibGroupSocket must be installed as own addon.",
+	POC_OPTIONS_USE_SORTING_LABEL =      "Sort lists by ultimate progress",
+	POC_OPTIONS_USE_SORTING_TOOLTIP =    "If activated, all lists will be sorted by ultimate progress (Maximum on top).",
+	POC_OPTIONS_STYLE_LABEL =            "Choose style",
+	POC_OPTIONS_STYLE_TOOLTIP =          "Choose your style: Standard or Compact",
+	POC_OPTIONS_STYLE_SWIM =             "Standard",
+	POC_OPTIONS_STYLE_SHORT_SWIM =       "Compact",
+	POC_OPTIONS_SWIMLANE_MAX_LABEL =     "Max number of ultimates to display in a swimlane",
+	POC_OPTIONS_ULTIMATE_NUMBER =        "Show your Ultimate number",
+	POC_OPTIONS_ULTIMATE_NUMBER_TOOLTIP ="Display your position in the list for your selected Ultimate",
+	POC_OPTIONS_WERE_NUMBER_ONE =        "Play a sound when you hit #1",
+	POC_OPTIONS_WERE_NUMBER_ONE_TOOLTIP ="Play a sound when you hit #1 position in the list for your selected Ultimate",
+    }
+
+    for id, val in pairs(strings) do
+	ZO_CreateStringId(id, val)
+	SafeAddVersion(id, 1)
+    end
+
     SLASH_COMMANDS["/rrr"] = function () ReloadUI() end
     SLASH_COMMANDS["/pocapi"] = function(n)
 	n = n:gsub("^%s*(.-)%s*$", "%1")
