83a2aa542a19b6250d85e9bf21015a026212630c
Chris Faylor <cgf@realedsolutions.com>
2019-03-19 21:59:04 -0400

    * POC.lua: Bump version to 3.36.

--- POC.lua
+++ POC.lua
@@ -3,7 +3,7 @@ Name = "POC"
 
 LANG = GetCVar("Language.2")
 
-local version = '3.35'
+local version = '3.36'
 local major = tonumber(version:match("^(%d+)"))
 local minor = tonumber(version:match("\.(%d+)"))
 local beta = tonumber(version:match("b(%d+)")) or '0'
